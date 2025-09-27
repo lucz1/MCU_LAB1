cb69aca29cec68efb613be7a917aa128a62c1ff7
lucz1 <huy.hoang010204@hcmut.edu.vn>
2025-09-27 14:35:31 +0700

    INIT EX1

--- Project/Core/Inc/main.h
+++ Project/Core/Inc/main.h
@@ -58,6 +58,12 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_RED_Pin GPIO_PIN_0
+#define LED_RED_GPIO_Port GPIOB
+#define LED_YELLOW_Pin GPIO_PIN_1
+#define LED_YELLOW_GPIO_Port GPIOB
+#define LED_GREEN_Pin GPIO_PIN_2
+#define LED_GREEN_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
